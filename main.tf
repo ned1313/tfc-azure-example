9f872e0042594b17db36469d7798295454b7ace9
ned1313 <ned.bellavance@gmail.com>
2022-05-19 06:38:42 -0400

    working

---
+++ main.tf
@@ -0,0 +1,33 @@
+variable "prefix" {
+  type        = string
+  description = "(Required) Naming prefix for resources."
+}
+
+variable "address_space" {
+  type        = string
+  description = "(Optional) Address space for virtual network, defaults to 10.0.0.0/16."
+  default     = "10.0.0.0/16"
+}
+
+variable "location" {
+  type        = string
+  description = "(Optional) Region for Azure resources, defaults to East US."
+  default     = "eastus"
+}
+
+locals {
+  base_name = "${var.prefix}web"
+}
+
+resource "azurerm_resource_group" "web" {
+  name     = local.base_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "web" {
+  name                = local.base_name
+  resource_group_name = azurerm_resource_group.web.name
+  location            = azurerm_resource_group.web.location
+
+  address_space = [var.address_space]
+}
