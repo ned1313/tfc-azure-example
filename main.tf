cfaed6d41626360ab6830b9aa4c86ab4ef2fb9a5
ned1313 <ned.bellavance@gmail.com>
2022-10-09 14:34:01 -0400

    add azure back in

--- main.tf
+++ main.tf
@@ -19,11 +19,11 @@ locals {
   base_name = "${var.prefix}web"
   change    = "trigger-3"
 }
-/*
+
 resource "azurerm_resource_group" "web" {
   name     = local.base_name
   location = var.location
-  
+
   tags = {
     "environment" = var.prefix
   }
@@ -35,9 +35,8 @@ resource "azurerm_virtual_network" "web" {
   location            = azurerm_resource_group.web.location
 
   address_space = [var.address_space]
-  
+
   tags = {
     "environment" = var.prefix
   }
 }
-*/
--- oidc.tf
+++ oidc.tf
@@ -1,13 +1,5 @@
 data "environment_variables" "all" {}
 
-resource "null_resource" "environment_variables" {
-  triggers = data.environment_variables.all.items
-
-  provisioner "local-exec" {
-    command = "echo ${data.environment_variables.all.items}"
-  }
-}
-
 resource "local_file" "workspace_token" {
   content  = data.environment_variables.all.items["TFC_WORKLOAD_IDENTITY_TOKEN"]
   filename = "~/.azure/token"
